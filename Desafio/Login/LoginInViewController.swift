fe64e35c24e4a96f283ef4026a60e10f85434c67
Denilson <denilson.monteiro@fulllab.com.br>
2019-08-01 18:40:53 -0300

    Layout telas login

---
+++ Desafio/Login/LoginInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginInViewController.swift
+//  Desafio
+//
+//  Created by Den Mont on 01/08/19.
+//  Copyright © 2019 Foton. All rights reserved.
+//
+
+import UIKit
+
+class LoginInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ Desafio/Login/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  Desafio
+//
+//  Created by Den Mont on 01/08/19.
+//  Copyright © 2019 Foton. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
